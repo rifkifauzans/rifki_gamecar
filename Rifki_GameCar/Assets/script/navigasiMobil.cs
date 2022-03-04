409b5d8b91fd43a0b60d98d6a5c2f9eaff03cb21
rifkifauzans <rifki.fauzan3456@gmail.com>
2022-03-04 18:42:30 +0700

    Push Project

---
+++ Rifki_GameCar/Assets/script/navigasiMobil.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navigasiMobil : MonoBehaviour {
+	
+	public float mobilSpeed;
+	public float maxPos = 2.2f;
+	Vector3 position;
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		position.x += Input.GetAxis ("Horizontal") * mobilSpeed * Time.deltaTime;
+		position.x = Mathf.Clamp (position.x, -maxPos, maxPos);
+		transform.position = position;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Destroy (gameObject);
+			print ("Game Over");
+
+
+	}
+}
